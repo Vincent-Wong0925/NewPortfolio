9faa713aab2cd2374a533bacd12862a7c3a5cd81
Vincent Wong <vincentwco2002@gmail.com>
2025-04-22 15:48:08 +0800

    Implement contact page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import './App.css';
 import Root from './components/root';
 import AboutPage from './pages/aboutPage';
+import ContactPage from './pages/contactPage';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Root />} >
     <Route index element={<></>} />
     <Route path='/about' element={<AboutPage />} />
+    <Route path='/contact' element={<ContactPage />} />
   </Route>
 ));
 
