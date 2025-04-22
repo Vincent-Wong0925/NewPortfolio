7571c8b716ef65e7fe024804be339865b0c4cfd0
Vincent Wong <vincentwco2002@gmail.com>
2025-04-22 14:30:58 +0800

    Create footer

---
+++ src/components/footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router";
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container className="p-4 mt-3 bg-dark text-white" fluid>
+                <Row>
+                    <Col>
+                        <h2>Vincent Wong</h2>
+                    </Col>
+                    <Col>
+                        <h3>Useful Links</h3>
+                        <Link className="my-2 text-reset text-decoration-none d-block" to="/">Home</Link>
+                        <Link className="my-2 text-reset text-decoration-none d-block" to="/about">About</Link>
+                        <Link className="my-2 text-reset text-decoration-none d-block" to="/contact">Contact</Link>
+                    </Col>
+                    <Col>
+                        <h3>Contact Me</h3>
+                        <p>Email: vincent.wco@gmail.com</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer;
