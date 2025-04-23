f28a8805c2f226412d0959cb891989465e7de724
Vincent Wong <vincentwco2002@gmail.com>
2025-04-23 15:54:59 +0800

    Implement banner

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Root from './components/root';
 import AboutPage from './pages/aboutPage';
 import ContactPage from './pages/contactPage';
+import HomePage from './pages/homePage';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Root />} >
-    <Route index element={<></>} />
+    <Route index element={<HomePage />} />
     <Route path='/about' element={<AboutPage />} />
     <Route path='/contact' element={<ContactPage />} />
   </Route>
---
+++ src/pages/homePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../components/banner";
+
+const HomePage = () => {
+    return (
+        <div className="homePage min-vh-100">
+            <Banner />
+        </div>
+    )
+}
+
+export default HomePage;
