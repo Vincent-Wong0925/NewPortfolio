cb21e790773191ac301277d86f6122933d980218
Vincent Wong <vincentwco2002@gmail.com>
2025-04-25 11:41:44 +0800

    Add image of e-commercce site

--- src/components/projects.js
+++ src/components/projects.js
@@ -24,10 +24,7 @@ const Projects = () => {
 
                 <Col className="d-flex">
                     <Card className="p-3 shadow">
-                        <svg className="bi bi-person-circle" height="20rem" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                        </svg>
+                        <Card.Img src={require('../img/e-commerce.png')} />
                         <Card.Body>
                             <Card.Title as="h3">E-commerce Site</Card.Title>
                             <Card.Text>React | Bootstrap | JavaScript | Node.js | Express.js | PostgreSQL</Card.Text>
