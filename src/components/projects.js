b6ee71701d12b74c0e5320629c2ac39b3756dee9
Vincent Wong <vincentwco2002@gmail.com>
2025-04-25 11:03:51 +0800

    Add github link to take a look button

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
                             <Card.Title as="h3">Portfolio Website</Card.Title>
                             <Card.Text>React | Bootstrap | JavaScript</Card.Text>
                             <Card.Text>A website built with React to showcase the projects I have done. It is built with React and decorated with Bootstrap.</Card.Text>
-                            <Button>Take a look</Button>
+                            <Button href="https://github.com/Vincent-Wong0925/NewPortfolio" target="_blank">Take a look</Button>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -32,7 +32,7 @@ const Projects = () => {
                             <Card.Title as="h3">E-commerce Site</Card.Title>
                             <Card.Text>React | Bootstrap | JavaScript | Node.js | Express.js | PostgreSQL</Card.Text>
                             <Card.Text>An e-commerce website that allows users to register and sign in an account, view available products and add products to the shopping cart. It is a PERN application with a front-end built with React, back-end built with Node.js and Express.js, and backed by a PostgreSQL database.</Card.Text>
-                            <Button>Take a look</Button>
+                            <Button href="https://github.com/Vincent-Wong0925/e-commerce-project" target="_blank">Take a look</Button>
                         </Card.Body>
                     </Card>
                 </Col>
@@ -44,7 +44,7 @@ const Projects = () => {
                             <Card.Title as="h3">Reddit Clone</Card.Title>
                             <Card.Text>React | Redux | CSS | JavaScript</Card.Text>
                             <Card.Text>A simple remake of the popular forum Reddit. This application allows user to browse and search for reddit posts in different subreddit. It is an front-end only application built with React and CSS and uses Redux to handle states.</Card.Text>
-                            <Button>Take a look</Button>
+                            <Button href="https://github.com/Vincent-Wong0925/reddit-clone" target="_blank">Take a look</Button>
                         </Card.Body>
                     </Card>
                 </Col>
